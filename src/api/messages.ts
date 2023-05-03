05ce714ce89331b17690480be4928e8729c92ba5
Antoine CORDIERgst <com.acordier@gmail.com>
2023-05-03 18:39:54 +0200

    fix: use shorter message names

--- src/api/messages.ts
+++ src/api/messages.ts
@@ -20,23 +20,23 @@ import { Status } from "./status.js";
  * Shorthand types for the different MIDI messages.
  */
 export namespace Messages {
-    export type ControlChangeMessage = Message<Status.CONTROL_CHANGE>;
-    export type NoteMessage = Message<Status.NOTE_ON | Status.NOTE_OFF>;
-    export type NoteOnMessage = Message<Status.NOTE_ON>;
-    export type NoteOffMessage = Message<Status.NOTE_OFF>;
-    export type PitchBendMessage = Message<Status.PITCH_BEND>;
-    export type NoteAfterTouchMessage = Message<Status.NOTE_AFTER_TOUCH>;
-    export type ChannelAfterTouchMessage = Message<Status.CHANNEL_AFTER_TOUCH>;
-    export type ProgramChangeMessage = Message<Status.PROGRAM_CHANGE>;
-    export type SysexMessage = Message<Status.SYSEX>;
-    export type TimingClockMessage = Message<Status.TIMING_CLOCK>;
-    export type StartMessage = Message<Status.START>;
-    export type StopMessage = Message<Status.STOP>;
-    export type ContinueMessage = Message<Status.CONTINUE>;
-    export type ActiveSendingMessage = Message<Status.ACTIVE_SENDING>;
-    export type SystemResetMessage = Message<Status.SYSTEM_RESET>;
-    export type SongSelectMessage = Message<Status.SONG_SELECT>;
-    export type SongPositionMessage = Message<Status.SONG_POSITION>;
-    export type MTCMessage = Message<Status.MTC>;
-    export type TuneRequestMessage = Message<Status.TUNE_REQUEST>;
+    export type ControlChange = Message<Status.CONTROL_CHANGE>;
+    export type Note = Message<Status.NOTE_ON | Status.NOTE_OFF>;
+    export type NoteOn = Message<Status.NOTE_ON>;
+    export type NoteOff = Message<Status.NOTE_OFF>;
+    export type PitchBend = Message<Status.PITCH_BEND>;
+    export type NoteAfterTouch = Message<Status.NOTE_AFTER_TOUCH>;
+    export type ChannelAfterTouch = Message<Status.CHANNEL_AFTER_TOUCH>;
+    export type ProgramChange = Message<Status.PROGRAM_CHANGE>;
+    export type Sysex = Message<Status.SYSEX>;
+    export type TimingClock = Message<Status.TIMING_CLOCK>;
+    export type Start = Message<Status.START>;
+    export type Stop = Message<Status.STOP>;
+    export type Continue = Message<Status.CONTINUE>;
+    export type ActiveSending = Message<Status.ACTIVE_SENDING>;
+    export type SystemReset = Message<Status.SYSTEM_RESET>;
+    export type SongSelect = Message<Status.SONG_SELECT>;
+    export type SongPosition = Message<Status.SONG_POSITION>;
+    export type MTC = Message<Status.MTC>;
+    export type TuneRequest = Message<Status.TUNE_REQUEST>;
 }
